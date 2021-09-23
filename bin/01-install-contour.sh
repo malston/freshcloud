0c97f2d1def9ae8234bc9452ff299e740210d6a0
Mark Alston <malston@vmware.com>
2021-09-23 16:51:57 -0600

    Add function to include psps

--- bin/01-install-contour.sh
+++ bin/01-install-contour.sh
@@ -14,11 +14,27 @@ source "${__DIR}/../.env_development.sh" || die "Could not find '.env_developmen
 # shellcheck disable=SC1091
 source "${__DIR}/../components/kubernetes-support/kubectl-support.sh" || die "Could not find 'kubectl-support.sh' in ${__DIR}/../components/kubernetes-support directory"
 
+function configure_psps() {
+    # allows the deployment of privileged workloads in default namespace
+    kubectl create rolebinding rolebinding-default-privileged-sa-ns_default \
+      --clusterrole=psp:vmware-system-privileged \
+      --group=system:serviceaccounts \
+      --namespace=default
+    # grants system:serviceaccounts cluster-wide access to run a privileged workloads
+    kubectl create clusterrolebinding clusterrolebinding-privileged-sa \
+      --clusterrole=psp:vmware-system-privileged \
+      --group=system:serviceaccounts
+    # grants system:serviceaccounts cluster-wide access to run a restricted set of workloads
+    kubectl create rolebinding psp:serviceaccounts \
+      --clusterrole=psp:vmware-system-restricted \
+      --group=system:serviceaccounts
+}
+
 function helm_install_contour() {
-  create_namespace "projectcontour"
-  helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm repo update
-  helm upgrade -i ingress bitnami/contour -n projectcontour --version 3.3.1 || die "Failed to install Contour. Bummer"
+    create_namespace "projectcontour"
+    helm repo add bitnami https://charts.bitnami.com/bitnami
+    helm repo update
+    helm upgrade -i ingress bitnami/contour -n "projectcontour"  --version 3.3.1 || die "Failed to install Contour."
 }
 
 function get_load_balancer_ip() {
@@ -34,5 +50,6 @@ function get_load_balancer_ip() {
   done
 }
 
+configure_psps
 helm_install_contour
 get_load_balancer_ip
